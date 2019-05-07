006b0cde020766f0ffd22640bc86313882283e74
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-05-07 21:04:00 +0200

    Inits project

---
+++ src/test/groovy/filli/ExampleTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package filli
+
+import filli.Example
+
+/**
+ * Tests for the {@link Example} class.
+ */
+class ExampleTest
+    extends GroovyTestCase
+{
+    void testShow() {
+        new Example().show()
+    }
+}
---
+++ src/test/groovy/filli/HelperTest.groovy
@@ -0,0 +1,19 @@
+//
+// Generated from archetype; please customize.
+//
+
+package filli
+
+import filli.Helper
+import filli.Example
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
